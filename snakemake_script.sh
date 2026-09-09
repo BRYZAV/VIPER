4c42b0f422edca7d7d584dd878b56f4ed63162ad
Bryan Zavala <97570002+BRYZAV@users.noreply.github.com>
2026-09-09 10:03:44 -0500

    Modify SLURM job parameters in snakemake_script.sh
    Updated job parameters and email notification settings.

--- snakemake_script.sh
+++ snakemake_script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=snakemake_bats
-#SBATCH -A zedru
-#SBATCH -p ceres
 #SBATCH -N 1
 #SBATCH -n 96
 #SBATCH --mem=2000G
 #SBATCH -t 300:00:00
-#SBATCH --mail-user=bryan.zavalamartinez@usda.gov
+#SBATCH --mail-user=first.lastname@usda.gov
 #SBATCH --mail-type=BEGIN,END,FINAL
 #SBATCH -o "stdout/stdout.snake.%j"
 #SBATCH -e "stderr/stderr.snake.%j"
